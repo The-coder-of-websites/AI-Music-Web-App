ae4d248e68db45a67b1b8a2d23be1dbce0531c57
The-coder-of-websites <89913566+The-coder-of-websites@users.noreply.github.com>
2022-05-26 17:55:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music_1="";
 music_2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload(){
   music_1= loadSound("music.mp3");
@@ -13,9 +17,30 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,550);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized!");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist x= "+leftWristX+" left wrist y= "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist x= "+rightWristX+" right wrist y= "+rightWristY);
+    }
 }
 
